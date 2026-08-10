bffb5361755cabf15412add10134e086da3d7660
Daemontail <gurtejs169@gmail.com>
2026-08-10 19:57:47 +0530

    Added dynamic blog rendering

---
+++ data.js
@@ -0,0 +1,48 @@
+ const blogEls=[
+    
+    {
+        name:'Blog One',
+        date:'July 23rd 2022',
+        description:`I'm excited to start a new journey as a Scrimba bootcamp student! After several months of learning
+        in the Frontend Developer Career Path.`,
+        image: "./images/article-image-01.png"
+    },
+    {
+        name:'Blog Two',
+        date:'July 23rd 2022',
+        description:`I'm excited to start a new journey as a Scrimba bootcamp student! After several months of learning
+        in the Frontend Developer Career Path.`,
+        image: "./images/article-image-02.png"
+    },
+    {
+        name:'Blog Three',
+        date:'July 23rd 2022',
+        description:`I'm excited to start a new journey as a Scrimba bootcamp student! After several months of learning
+        in the Frontend Developer Career Path.`,
+        image: "./images/article-image-03.png"
+    },
+    {
+        name:'Blog Four',
+        date:'July 23rd 2022',
+        description:`I'm excited to start a new journey as a Scrimba bootcamp student! After several months of learning
+        in the Frontend Developer Career Path.`,
+        image: "./images/article-image-04.png"
+    },
+    {
+        name:'Blog Five',
+        date:'July 23rd 2022',
+        description:`I'm excited to start a new journey as a Scrimba bootcamp student! After several months of learning
+        in the Frontend Developer Career Path.`,
+        image: "./images/article-image-05.png"
+    },
+    {
+        name:'Blog Six',
+        date:'July 23rd 2022',
+        description:`I'm excited to start a new journey as a Scrimba bootcamp student! After several months of learning
+        in the Frontend Developer Career Path.`,
+        image: "./images/article-image-06.png"
+    },
+
+]
+
+export default blogEls;
---
+++ script.js
@@ -0,0 +1,33 @@
+import blogEls from "./data.js"
+let currBlogs = 0
+document.addEventListener('click', function (e) {
+    if (e.target.id === 'view-more') {
+        return rendermoreblogs()
+    }
+})
+
+
+function rendermoreblogs() {
+    if(currBlogs < blogEls.length){
+    const blogcount = currBlogs + 3
+    const main = document.getElementById('main')
+    while (blogcount > currBlogs) {
+        const {name,date,description,image} = blogEls[currBlogs]
+        main.innerHTML += `
+        <article class="article">
+            <a href="#">
+            <img class="article-img" src="${image}">
+            <p class="date">${date}</p>
+            <h2 class="title">${name}</h2>
+            <p class="blog-text">${description}</p>
+            </a>
+        </article>
+            `
+        currBlogs++
+        if(currBlogs === blogEls.length){
+            document.getElementById('view-more').textContent=''
+        }
+    }
+}
+}
+rendermoreblogs()
